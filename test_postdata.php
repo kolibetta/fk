962fae1eae287ab7105087b4f9ec4cab75ac26f8
kolibetta <savakarun@gmail.com>
2018-03-13 17:32:52 +0530

    Add files via upload

--- test_postdata.php
+++ test_postdata.php
@@ -1,5 +1,26 @@
 <?php
 
+	$url = "https://maps.google.com/maps/api/geocode/json?address=mysore&sensor=false&region=India&key=AIzaSyBtmiAuZQaFAgfssrfwtyFEQhY2NWzkgJ0";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	$response_a = json_decode($response);
+	$lat = $response_a->results[0]->geometry->location->lat;
+	$long = $response_a->results[0]->geometry->location->lng;
+	
+	echo "Lat -".$lat."<br>";
+	echo "long -".$long."<br>";
+	
+	
+	
+	
+	
+
 	$data = file_get_contents("https://maps.google.com/maps/api/geocode/json?latlng=8.088306,77.538451&key=AIzaSyBtmiAuZQaFAgfssrfwtyFEQhY2NWzkgJ0&callback=initMap");
 	$data = json_decode($data);
 	$add_array  = $data->results;
